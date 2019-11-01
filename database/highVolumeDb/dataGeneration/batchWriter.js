1a1d4691abbe968f9d24c0b1e496e55385000574
Mike Flinn <mflinn92@gmail.com>
2019-10-31 23:11:57 -0400

    Removed console logs from data generator to increase speed

--- database/highVolumeDb/dataGeneration/batchWriter.js
+++ database/highVolumeDb/dataGeneration/batchWriter.js
@@ -2,7 +2,7 @@ const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
 module.exports = async (records, batchNumber, csvWriter) => {
   await csvWriter.writeRecords(records);
-  if (batchNumber % 100 === 0 ) {
-    console.log(`Wrote batch number: ${batchNumber}`);
-  }
+  // if (batchNumber % 100 === 0 ) {
+  //   console.log(`Wrote batch number: ${batchNumber}`);
+  // }
 };
