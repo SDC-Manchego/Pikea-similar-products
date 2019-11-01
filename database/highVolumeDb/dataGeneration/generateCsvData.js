a6144b19fb0d5c1fe6fdcba1fc558c721e037670
Mike Flinn <mflinn92@gmail.com>
2019-11-01 09:22:48 -0400

    REfactored functions calls to start writing files to be async so that the files are written one after another rather than simultaneously.

--- database/highVolumeDb/dataGeneration/generateCsvData.js
+++ database/highVolumeDb/dataGeneration/generateCsvData.js
@@ -1,8 +1,9 @@
 const writeReviewsToCsv = require('./writeReviewsToCsv.js');
 const writeProductsToCsv = require('./writeProductsToCsv.js');
 
-writeReviewsToCsv();
-writeProductsToCsv();
-
+async () => {
+  await writeReviewsToCsv();
+  await writeProductsToCsv();
+}();
 // Total products created: 35 Mil
 // Total Runtime: 7min 15sec
