3116edddd6652678a6c54395a76e656735bb3fe9
Mike Flinn <mflinn92@gmail.com>
2019-11-02 17:28:52 -0400

    Used templete string to make copy query a little more readable. Could not use parameterized query as this seems to not be supported for COPY in postgres

--- database/highVolumeDb/postgres/importFromCsv.js
+++ database/highVolumeDb/postgres/importFromCsv.js
@@ -9,9 +9,10 @@ let client;
     password: 'beckypete0206'
   });
   client = await pool.connect();
-  const productPath = [path.join(__dirname, '../','dataGeneration', 'csvs', 'products.csv')];
+  // had to hard code windows path because wsl uses linux path syntax then postgres can't find file
+  const productPath = 'D:\\hackreactor\\SDC\\djason-ikea-similar-products\\database\\highVolumeDb\\dataGeneration\\csvs\\products.csv';
   // console.log(productPath);
-  const productInsert = 'COPY similar_products (title_similar, desc_similar, price_similar, img_similar, created_similar, category_similar) FROM \'D:\\hackreactor\\SDC\\djason-ikea-similar-products\\database\\highVolumeDb\\dataGeneration\\csvs\\products.csv\' WITH DELIMITER \',\' CSV HEADER' ;
+  const productInsert = `COPY similar_products (title_similar, desc_similar, price_similar, img_similar, created_similar, category_similar) FROM '${productPath}' DELIMITEr \',\' CSV HEADER` ;
   try {
     const res = await client.query(productInsert);
     console.log('insert complete');
